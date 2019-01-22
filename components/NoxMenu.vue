018c2deb3da041e506e306322d8c1122f4815ff2
Michael Rice <rice.michaelt@gmail.com>
2019-01-22 12:27:27 -0600

    initial commit

---
+++ components/NoxMenu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-navigation-drawer :clipped="clipped" v-model="drawer" fixed app>
+      <v-list>
+        <v-list-tile v-for="(item, i) in items" :to="item.to" :key="i" router exact>
+          <v-list-tile-action>
+            <v-icon v-html="item.icon" />
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-title v-text="title"/>
+    </v-toolbar> 
+  </div>
+</template>
+
+  <script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          {title: 'NES'},
+          {title: 'SNES'},
+          {title: 'N64'}
+        ],
+        title: 'Video Game Collection'
+      }
+    }
+  }
+  /*
+          items: []
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+  */
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <nox-menu />
+    <v-content>
+      <v-container>
+        <nuxt />
+    </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import NoxMenu from '~/components/NoxMenu.vue'
+
+  export default {
+    components: {
+      NoxMenu
+    },
+    data() {
+      return {
+        clipped: true,
+        drawer: true,
+        fixed: false,
+        miniVariant: false,
+        title: 'Video Game Collection'
+      }
+    }
+  }
+</script>
